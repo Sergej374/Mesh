6f364f2a4ae40f8517387b52a83b342a3351a1ac
Todly <furrykunstler@gmail.com>
2020-05-26 00:33:04 +0300

    radiuses, dragging, export, import

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,50 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.workPanel = new System.Windows.Forms.Panel();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.typeChooseButton = new System.Windows.Forms.ToolStripDropDownButton();
+            this.nodeTypeButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.sourceTypeButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.targetTypeButton = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.selectedIDLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.workPanel = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripSeparator1,
+            this.typeChooseButton});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(617, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // workPanel
+            // toolStripDropDownButton1
             // 
-            this.workPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.workPanel.Location = new System.Drawing.Point(0, 25);
-            this.workPanel.Name = "workPanel";
-            this.workPanel.Size = new System.Drawing.Size(800, 425);
-            this.workPanel.TabIndex = 1;
-            this.workPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.workPanel_Paint);
-            this.workPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.workPanel_MouseDown);
-            this.workPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.workPanel_MouseUp);
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // typeChooseButton
+            // 
+            this.typeChooseButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.typeChooseButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nodeTypeButton,
+            this.sourceTypeButton,
+            this.targetTypeButton});
+            this.typeChooseButton.Enabled = false;
+            this.typeChooseButton.Image = ((System.Drawing.Image)(resources.GetObject("typeChooseButton.Image")));
+            this.typeChooseButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.typeChooseButton.Name = "typeChooseButton";
+            this.typeChooseButton.Size = new System.Drawing.Size(44, 22);
+            this.typeChooseButton.Text = "Type";
+            // 
+            // nodeTypeButton
+            // 
+            this.nodeTypeButton.Name = "nodeTypeButton";
+            this.nodeTypeButton.Size = new System.Drawing.Size(110, 22);
+            this.nodeTypeButton.Text = "Node";
+            this.nodeTypeButton.Click += new System.EventHandler(this.nodeTypeButton_Click);
+            // 
+            // sourceTypeButton
+            // 
+            this.sourceTypeButton.Name = "sourceTypeButton";
+            this.sourceTypeButton.Size = new System.Drawing.Size(110, 22);
+            this.sourceTypeButton.Text = "Source";
+            this.sourceTypeButton.Click += new System.EventHandler(this.sourceTypeButton_Click);
+            // 
+            // targetTypeButton
+            // 
+            this.targetTypeButton.Name = "targetTypeButton";
+            this.targetTypeButton.Size = new System.Drawing.Size(110, 22);
+            this.targetTypeButton.Text = "Target";
+            this.targetTypeButton.Click += new System.EventHandler(this.targetTypeButton_Click);
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectedIDLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 381);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(617, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
+            // 
+            // selectedIDLabel
+            // 
+            this.selectedIDLabel.Name = "selectedIDLabel";
+            this.selectedIDLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // workPanel
+            // 
+            this.workPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.workPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.workPanel.Location = new System.Drawing.Point(0, 25);
+            this.workPanel.Name = "workPanel";
+            this.workPanel.Size = new System.Drawing.Size(617, 356);
+            this.workPanel.TabIndex = 3;
+            this.workPanel.SizeChanged += new System.EventHandler(this.workPanel_SizeChanged);
+            this.workPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.workPanel_Paint);
+            this.workPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.workPanel_MouseDown);
+            this.workPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.workPanel_MouseMove);
+            this.workPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.workPanel_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.statusStrip1);
+            this.ClientSize = new System.Drawing.Size(617, 403);
             this.Controls.Add(this.workPanel);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mesh-network";
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,8 +175,17 @@
         #endregion
 
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.Panel workPanel;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel selectedIDLabel;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Panel workPanel;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripDropDownButton typeChooseButton;
+        private System.Windows.Forms.ToolStripMenuItem nodeTypeButton;
+        private System.Windows.Forms.ToolStripMenuItem sourceTypeButton;
+        private System.Windows.Forms.ToolStripMenuItem targetTypeButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,20 +15,26 @@ namespace Mesh
     {
         static int ID = 1;
         int selectedNodeID = -1;
-        List<Node> nodes = new List<Node>();
         private Graphics drawer, panelDrawer;
-        private Pen[] pens = new Pen[] { new Pen(Color.Blue), new Pen(Color.DeepPink), new Pen(Color.Green), new Pen(Color.Black) };
+        private Pen[] pens = new Pen[] { new Pen(Color.Blue, 3), new Pen(Color.DeepPink, 3), new Pen(Color.Green, 3), new Pen(Color.Black), new Pen(Color.DarkOrange, 3) };
         private int nodesSize = 50;
         private Bitmap picture;
         private bool mouseIsDown = false;
-        private int dragAccuracy = 50;
+        private bool draggingRadius = false;
+        private int dragAccuracy = 25;
 
-        public List<Node> Nodes
+        public List<Node> Nodes { get; set; } = new List<Node>();
+
+        public int SelectedNodeID
         {
-            get => nodes;
+            get => selectedNodeID;
             set
             {
-                nodes = value;
+                selectedNodeID = value;
+                if (selectedNodeID != -1)
+                    typeChooseButton.Enabled = true;
+                else
+                    typeChooseButton.Enabled = false;
             }
         }
 
@@ -53,17 +60,19 @@ namespace Mesh
 
             foreach (Node node in Nodes)
             {
-                if (node.ID != selectedNodeID)
+                if (node.ID != SelectedNodeID)
                 {
-                    if (node.Type != "Target")
+                    if (node.Type == "Node")
                         drawer.DrawEllipse(pens[0], new Rectangle(node.Position.X - nodesSize / 2, node.Position.Y - nodesSize / 2, nodesSize, nodesSize));
+                    else if (node.Type == "Source")
+                        drawer.DrawEllipse(pens[4], new Rectangle(node.Position.X - nodesSize / 2, node.Position.Y - nodesSize / 2, nodesSize, nodesSize));
                     else
                         drawer.DrawEllipse(pens[1], new Rectangle(node.Position.X - nodesSize / 2, node.Position.Y - nodesSize / 2, nodesSize, nodesSize));
                 }
                 else
                 {
-                    drawer.DrawEllipse(pens[1], new Rectangle(node.Position.X - nodesSize / 2, node.Position.Y - nodesSize / 2, nodesSize, nodesSize));
-                    drawer.DrawEllipse(pens[3], new Rectangle(node.Position.X - node.Radius / 2, node.Position.Y - node.Radius / 2, node.Radius, node.Radius));
+                    drawer.DrawEllipse(pens[2], new Rectangle(node.Position.X - nodesSize / 2, node.Position.Y - nodesSize / 2, nodesSize, nodesSize));
+                    drawer.DrawEllipse(pens[3], new Rectangle(node.Position.X - node.Radius, node.Position.Y - node.Radius, node.Radius * 2, node.Radius * 2));
                 }
             }
             workPanel.Invalidate();
@@ -74,42 +83,183 @@ namespace Mesh
             mouseIsDown = true;
             for (int i = 0; i < Nodes.Count; i++)
             {
-                double distanceToMouse = Math.Sqrt(Math.Pow(e.X - nodes[i].Position.X, 2) + Math.Pow(e.Y - nodes[i].Position.Y, 2));
+                double distanceToMouse = Math.Sqrt(Math.Pow(e.X - Nodes[i].Position.X, 2) + Math.Pow(e.Y - Nodes[i].Position.Y, 2));
                 if (distanceToMouse <= dragAccuracy)
                 {
-                    selectedNodeID = nodes[i].ID;
+                    SelectedNodeID = Nodes[i].ID;
+                    selectedIDLabel.Text = "ID: " + SelectedNodeID;
+                    typeChooseButton.Text = Nodes.Find(Node => Node.ID == selectedNodeID).Type;
+                    typeChooseButton.Enabled = true;
+                    UpdateMesh();
+                    return;
+                }
+                else if (Math.Abs(distanceToMouse - Nodes[i].Radius) < 2)
+                {
+                    draggingRadius = true;
+                    Nodes[i].Radius = Convert.ToInt32(distanceToMouse);
                     UpdateMesh();
                     return;
                 }
             }
-            selectedNodeID = -1;
+
+            if (SelectedNodeID != -1 && !draggingRadius)
+            {
+                SelectedNodeID = -1;
+                selectedIDLabel.Text = "";
+                typeChooseButton.Enabled = false;
+                UpdateMesh();
+                return;
+            }
 
             Point mousePos = e.Location;
-            if (selectedNodeID == -1)
+            if (SelectedNodeID == -1)
                 AddNode(mousePos, 100, "Node");
 
             workPanel.Invalidate();
         }
 
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        private void workPanel_MouseUp(object sender, MouseEventArgs e)
         {
-            Text = MousePosition.ToString();
+            draggingRadius = false;
+            mouseIsDown = false;
+            workPanel.Invalidate();
+        }
 
-            if (mouseIsDown)
+        private void workPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < Nodes.Count; i++)
             {
-                nodes.Find(Node => Node.ID == selectedNodeID).Position = e.Location;
-                workPanel.Invalidate();
+                double distanceToMouse = Math.Sqrt(Math.Pow(e.X - Nodes[i].Position.X, 2) + Math.Pow(e.Y - Nodes[i].Position.Y, 2));
+
+                if (distanceToMouse <= nodesSize / 2)
+                {
+                    Cursor.Current = Cursors.SizeAll;
+                }
+                else if (Nodes[i].ID == selectedNodeID && Math.Abs(distanceToMouse - Convert.ToDouble(Nodes[i].Radius)) < 2)
+                {
+                    Cursor.Current = Cursors.Cross;
+                }
+            }
+
+
+            if (mouseIsDown && SelectedNodeID != -1)
+            {
+                if (Cursor.Position.X < Left)
+                    Cursor.Position = new Point(Left, Cursor.Position.Y);
+                if (Cursor.Position.X > Right)
+                    Cursor.Position = new Point(Right, Cursor.Position.Y);
+                if (Cursor.Position.Y < Top + workPanel.Top + toolStrip1.Height)
+                    Cursor.Position = new Point(Cursor.Position.X, Top + workPanel.Top + toolStrip1.Height);
+                if (Cursor.Position.Y > Top + workPanel.Bottom)
+                    Cursor.Position = new Point(Cursor.Position.X, Top + workPanel.Bottom);
+
+                Node nod = Nodes.Find(Node => Node.ID == SelectedNodeID);
+                double distanceToMouse = Math.Sqrt(Math.Pow(e.X - nod.Position.X, 2) + Math.Pow(e.Y - nod.Position.Y, 2));
+                if (draggingRadius)
+                {
+                    if (distanceToMouse >= nodesSize / 2)
+                        Nodes.Find(Node => Node.ID == SelectedNodeID).Radius = Convert.ToInt32(distanceToMouse);
+                }
+                else
+                {
+                    Nodes.Find(Node => Node.ID == SelectedNodeID).Position = e.Location;
+                }
+
+                UpdateMesh();
+            }
+
+
+        }
+
+        private void workPanel_SizeChanged(object sender, EventArgs e)
+        {
+            Bitmap bitmap = picture;
+            picture = new Bitmap(workPanel.Width, workPanel.Height);
+            drawer = Graphics.FromImage(picture);
+            panelDrawer = workPanel.CreateGraphics();
+            drawer.DrawImage(bitmap, 0, 0);
+            workPanel.Invalidate();
+        }
+
+        private void nodeTypeButton_Click(object sender, EventArgs e)
+        {
+            Nodes.Find(Node => Node.ID == selectedNodeID).Type = "Node";
+            typeChooseButton.Text = "Node";
+            UpdateMesh();
+        }
+
+        private void sourceTypeButton_Click(object sender, EventArgs e)
+        {
+            Nodes.Find(Node => Node.ID == selectedNodeID).Type = "Source";
+            typeChooseButton.Text = "Source";
+            UpdateMesh();
+        }
+
+        private void targetTypeButton_Click(object sender, EventArgs e)
+        {
+            Nodes.Find(Node => Node.ID == selectedNodeID).Type = "Target";
+            typeChooseButton.Text = "Target";
+            UpdateMesh();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string output = "";
+            output += ID + "\n";
+            foreach (Node node in Nodes)
+            {
+                output += node.Position.X + " " + node.Position.Y + " " + node.Radius + " ";
+                switch (node.Type)
+                {
+                    case "Node":
+                        output += 1;
+                        break;
+                    case "Source":
+                        output += 2;
+                        break;
+                    case "Target":
+                        output += 3;
+                        break;
+                }
+                output += " " + node.ID + "\n";
+            }
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "txt|.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(dialog.FileName, output);
             }
         }
 
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-        }
+            OpenFileDialog dialog = new OpenFileDialog();
+            string[] input = new string[2];
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Nodes.Clear();
+                input = File.ReadAllLines(dialog.FileName);
 
-        private void workPanel_MouseUp(object sender, MouseEventArgs e)
-        {
-            mouseIsDown = false;
-            workPanel.Invalidate();
+                for (int i = 1; i < input.Length; i++)
+                {
+                    string[] nums = input[i].Split(new char[] { ' ' });
+                    Nodes.Add(new Node(new Point(Convert.ToInt32(nums[0]), Convert.ToInt32(nums[1])), Convert.ToInt32(nums[2]), "type", Convert.ToInt32(nums[4])));
+                    switch (nums[3])
+                    {
+                        case "1":
+                            Nodes[Nodes.Count - 1].Type = "Node";
+                            break;
+                        case "2":
+                            Nodes[Nodes.Count - 1].Type = "Source";
+                            break;
+                        case "3":
+                            Nodes[Nodes.Count - 1].Type = "Target";
+                            break;
+                    }
+                }
+                ID = Convert.ToInt32(input[0]);
+            }
+            UpdateMesh();
         }
 
         private void workPanel_Paint(object sender, PaintEventArgs e)
